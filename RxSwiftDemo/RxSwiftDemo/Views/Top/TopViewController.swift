eeab3e8937a05bb445a68613dde3a747634a2a10
r-miyauchi <rmiyauchi@opendoor.co.jp>
2020-01-21 11:03:41 +0900

    初期状態で市リストを表示するように変更

--- RxSwiftDemo/RxSwiftDemo/Views/Top/TopViewController.swift
+++ RxSwiftDemo/RxSwiftDemo/Views/Top/TopViewController.swift
@@ -22,13 +22,18 @@ class TopViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     
     private let disposeBag = DisposeBag()
+    
+    // 市リスト
     private var cities: [CityEntity] = []
+    // TableViewに表示する市リストの検索結果
+    private var searchedCities = BehaviorRelay<[CityEntity]>(value: [])
     
     private let dataSource = TopDataSource()
     
     private let observableExample = ObservableExample()
     
-    // UISearchBarのテキスト変更をRxで取得
+    // UISearchBarのテキスト変更イベントをRxで取得
+    // 入力完了まで200msまで待つ、ということもRxで実装できる
     private var searchBarChangeText: Driver<String> {
         return rx
             .methodInvoked(#selector(UISearchBarDelegate.searchBar(_:shouldChangeTextIn:replacementText:)))
@@ -42,13 +47,24 @@ class TopViewController: UIViewController {
         super.viewDidLoad()
         
         // インクリメントサーチ処理
-        // Rxで通知されたテキストを使って検索し、TableViewの表示項目を更新する
-        // ここでDriverの設定をすると実行時にWarningが出てしまうが、Rxライブラリの修正待ち
+        // ここでは1.SearchBar--(バインド)-->searchedCities, 2.searchedCities--(バインド)-->tableViewを実施
+        // ※ viewDidLoadでDriverの設定をすると実行時にWarningが出てしまうが、Rxライブラリの修正待ち
+        // 1. Rxで通知されたテキストを使って検索し、searchedCitiesをRxで書き換える
+        //     flatMapで検索条件に合った市リストのDriverに変換し、driveでsearchCitiesにバインド
         searchBarChangeText
             .flatMap { [weak self] text -> Driver<[CityEntity]> in
                 guard let me = self else { return .just([]) }
+                if text.isEmpty {
+                    return .just(me.cities)
+                }
                 return .just(me.cities.filter { $0.containWord(text) })
             }
+            .drive(searchedCities)
+            .disposed(by: disposeBag)
+        // 2. ここでは検索結果リストとTableViewをバインド
+        //    こうすることで、searchCitiesの値を直接変更してもTableViewが更新するようになる
+        searchedCities
+            .asDriver()
             .drive(tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
     }
@@ -77,7 +93,7 @@ class TopViewController: UIViewController {
 
 extension TopViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let city = cities[indexPath.row]
+        let city = searchedCities.value[indexPath.row]
         self.performSegue(withIdentifier: TopConstant.showForecastSeque, sender: city)
     }
 }
@@ -101,7 +117,8 @@ private extension TopViewController {
             disposeBag: disposeBag,
             onSuccess: { (cities) in
                 self.cities = cities
-                self.tableView.reloadData()
+                // TableViewに全件表示したいので、tableViewにバインドしているsearchedCitiesを更新
+                self.searchedCities.accept(cities)
             },
             onError: { (error) in
                 print(error)
@@ -109,7 +126,7 @@ private extension TopViewController {
     }
     
     func runExamples() {
-//        observableExample.start()
-        observableExample.startWithMap()
+        observableExample.start()
+//        observableExample.startWithMap()
     }
 }
